9f4a96cf14a7b690e6a59b437aad30bd93fdcc42
Salem 0515 <mohamadhussen565@gmail.com>
2025-01-17 22:16:23 +0200

    add: deploy workflows

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home/Home'
-import { SigninSignup } from './Components/SigninSignup/SigninSignup'
-import Rooms from './Components/Rooms/Rooms'
+import Home from './Components/Home/Home';
+import { SigninSignup } from './Components/SigninSignup/SigninSignup';
+import Rooms from './Components/Rooms/Rooms';
 
 const App = () => {
   return (
     <div>
-      <Router >
+      <Router basename="/Bondi_Hotel">
         <Routes>
-          <Route basename="/Bondi_Hotel" path="/" element={<Home />} />
-          <Route basename="/Bondi_Hotel" path="/signin-signup" element={<SigninSignup />} />
-          <Route basename="/Bondi_Hotel" path="/rooms" element={<Rooms />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin-signup" element={<SigninSignup />} />
+          <Route path="/rooms" element={<Rooms />} />
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
