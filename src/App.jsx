257bbbfbab0bb064dc63bd3903a9febb4eac3807
Salem0515 <167808611+Salem0515@users.noreply.github.com>
2025-01-17 22:31:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import { SigninSignup } from './Components/SigninSignup/SigninSignup';
 import Rooms from './Components/Rooms/Rooms';
+
+const Navigation = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/signin-signup">Sign In/Sign Up</Link>
+    <Link to="/rooms">Rooms</Link>
+  </nav>
+);
 
 const App = () => {
   return (
     <div>
       <Router basename="/Hotel">
+        <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin-signup" element={<SigninSignup />} />
